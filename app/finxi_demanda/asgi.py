f1068443c2c3510de20057213447e77c549f8b8f
Eduardo-Lucas <eduardolucas40gmail.com>
2020-12-08 19:44:30 -0300

    first commit

---
+++ app/finxi_demanda/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for finxi_demanda project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'finxi_demanda.settings')
+
+application = get_asgi_application()
